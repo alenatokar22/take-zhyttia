afe8b9f07cf0aaf6c33c0854ba2814489edfa345
Alyona <alenatokar22@gmail.com>
2026-02-23 13:41:18 +0200

    Initial commit: Take Zhyttia cooking blog

---
+++ src/data/config.js
@@ -0,0 +1,5 @@
+export const channel = {
+  name: "Таке життя",
+  // Встав сюди посилання на твій канал, наприклад: "https://www.youtube.com/@YourHandle"
+  youtubeUrl: "https://www.youtube.com/",
+}
